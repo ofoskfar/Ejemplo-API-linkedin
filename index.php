d410ee6864246c6537e375a23b1eaa9685541e4d
CyDET <ofoskfar@users.noreply.github.com>
2018-10-03 12:18:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 <title>Login Linkendin</title>
 </head>
 <body>
-    <a href='https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77r4yi7dvtvrpe&redirect_uri=http://cursosim.com/respuesta.php&state=987654321'><img src="signin.png" /></a>
+    <a href='https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=XXXXXXXX&redirect_uri=XXXXXX&state=987654321'><img src="signin.png" /></a>
 </body>
 </html>
